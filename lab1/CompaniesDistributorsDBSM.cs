707b4527bb74df91db4ab5a03a27bc96a91f5e90
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-10-04 23:01:46 +0300

    lab1

---
+++ lab1/CompaniesDistributorsDBSM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab1
+{
+
+   public class CompaniesDistributorsDBSM //Цей клас представляє собою модель даних для зберігання інформації про компанії-дистриб'ютори
+{
+    public string company { get; set; } = ""; //Рядок, який зберігає ім'я компанії
+    public int products { get; set; } = 0; //Ціле число, яке представляє кількість продуктів компанії
+    public long asv { get; set; } = 0; //Довге ціле число, яке представляє річний обсяг продажів компанії
+    public double marketPart { get; set; } = 0; //Десяткове число, яке представляє частку ринку, зайняту компанією у відсотках
+
+
+    public CompaniesDistributorsDBSM() { } //Конструктор за замовчуванням
+
+    public CompaniesDistributorsDBSM(string company, int products, long asv, double marketPart) // Конструктор, який дозволяє встановити значення всіх полів при створенні об'єкта класу на основі переданих аргументів
+    {
+        this.company = company;
+        this.products = products;
+        this.asv = asv;
+        this.marketPart = marketPart;
+    }
+
+}
+
+}
---
+++ lab1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lab1
+{
+   
+    class Program
+    {
+        
+
+        static void Main(string[] args) //У даному методі створюються об'єкти класу CompaniesDistributorsDBSM, які представляють різні компанії-дистриб'ютори, ініціалізуються відповідними значеннями
+        {
+            CompaniesDistributorsDBSM Oracle = new CompaniesDistributorsDBSM("Oracle", 1, 2500000000, 31.01);
+            CompaniesDistributorsDBSM IBM = new CompaniesDistributorsDBSM("IBM", 3, 2400000000, 29.25);
+            CompaniesDistributorsDBSM Microsoft = new CompaniesDistributorsDBSM("Microsoft", 2, 1000000000, 13.01);
+            CompaniesDistributorsDBSM companiesDistributorsDBSM1 = new CompaniesDistributorsDBSM();
+
+
+        }
+
+
+    }
+}
