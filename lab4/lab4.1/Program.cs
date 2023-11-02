35ad52350ec05f3878abc3c16204104efdf29921
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-02 09:52:50 +0200

    lab4

---
+++ lab4/lab4.1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+
+    // Class Wheel
+    class Wheel
+    {
+        public string Model { get; set; }
+
+        public Wheel(string model)
+        {
+            Model = model;
+        }
+    }
+
+    // Class Engine
+    class Engine
+    {
+        public string Type { get; set; }
+
+        public Engine(string type)
+        {
+            Type = type;
+        }
+    }
+
+    // Class Car
+    class Car
+    {
+        public string Brand { get; set; }
+        public Wheel FrontWheel { get; set; }
+        public Wheel RearWheel { get; set; }
+        public Engine Engine { get; set; }
+
+        public Car(string brand, Wheel frontWheel, Wheel rearWheel, Engine engine)
+        {
+            Brand = brand;
+            FrontWheel = frontWheel;
+            RearWheel = rearWheel;
+            Engine = engine;
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("The car is driving.");
+        }
+
+        public void Refuel()
+        {
+            Console.WriteLine("The car is refueling.");
+        }
+
+        public void ChangeWheel(Wheel oldWheel, Wheel newWheel)
+        {
+            if (oldWheel == FrontWheel)
+            {
+                FrontWheel = newWheel;
+            }
+            else if (oldWheel == RearWheel)
+            {
+                RearWheel = newWheel;
+            }
+            Console.WriteLine("The wheel has been replaced.");
+        }
+
+        public void PrintBrand()
+        {
+            Console.WriteLine($"Car brand: {Brand}");
+        }
+    }
+
+    
+    public class Program
+    {
+        static void Main()
+        {
+            // Example usage
+            Wheel frontWheel = new Wheel("Summer");
+            Wheel rearWheel = new Wheel("Summer");
+            Wheel newWheel = new Wheel("Summer");
+            Engine engine = new Engine("Petrol");
+            Car car = new Car("Mercedes", frontWheel, rearWheel, engine);
+
+            car.Drive();
+            car.Refuel();
+            car.ChangeWheel(rearWheel,newWheel);
+            car.PrintBrand();
+        }
+    }
+    
---
+++ lab4/lab4.2/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+class TNumber
+{
+    protected string number;
+
+    public TNumber(string number)
+    {
+        this.number = number;
+    }
+
+    public virtual int SumOfDigits()
+    {
+        int sum = 0;
+        foreach (char digit in number)
+        {
+            if (char.IsDigit(digit))
+            {
+                sum += int.Parse(digit.ToString());
+            }
+        }
+        return sum;
+    }
+
+    public virtual char FirstDigit()
+    {
+        foreach (char digit in number)
+        {
+            if (char.IsDigit(digit))
+            {
+                return digit;
+            }
+        }
+        return '0';
+    }
+
+    public virtual char LastDigit()
+    {
+        char last = '0';
+        foreach (char digit in number)
+        {
+            if (char.IsDigit(digit))
+            {
+                last = digit;
+            }
+        }
+        return last;
+    }
+}
+
+class TIntNumber : TNumber
+{
+    public TIntNumber(string number) : base(number)
+    {
+    }
+
+    public override int SumOfDigits()
+    {
+        return base.SumOfDigits();
+    }
+}
+
+class TRealNumber : TNumber
+{
+    public TRealNumber(string number) : base(number)
+    {
+    }
+
+    public override char LastDigit()
+    {
+        char last = '0';
+        foreach (char digit in number)
+        {
+            if (char.IsDigit(digit))
+            {
+                last = digit;
+            }
+        }
+        return last;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int m = 5; 
+        int n = 5; 
+        int sumOfFirstDigits = 0;
+        int sumOfLastDigits = 0;
+
+        TIntNumber[] intNumbers = new TIntNumber[m];
+        TRealNumber[] realNumbers = new TRealNumber[n];
+
+        for (int i = 0; i < m; i++)
+        {
+            int randomInt = random.Next(1, 1000);
+            intNumbers[i] = new TIntNumber(randomInt.ToString());
+            sumOfFirstDigits += intNumbers[i].SumOfDigits();
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            double randomReal = random.NextDouble() * 1000;
+            realNumbers[i] = new TRealNumber(randomReal.ToString("0.00"));
+            sumOfLastDigits += int.Parse(realNumbers[i].LastDigit().ToString());
+        }
+
+        Console.WriteLine("Сума перших цифр цілих чисел: " + sumOfFirstDigits);
+        Console.WriteLine("Сума останніх цифр дійсних чисел: " + sumOfLastDigits);
+    }
+}
