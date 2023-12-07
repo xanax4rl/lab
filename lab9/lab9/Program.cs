75e5ab6b3f0bfb983550d3ba43afd4f5a46ba981
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-12-07 15:35:40 +0200

    lab9

---
+++ lab9/lab9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+// Делегат для підстановки функції, яку потрібно інтегрувати
+delegate double FunctionDelegate(double x);
+
+class IntegralCalculator
+{
+    // Метод для обчислення визначеного інтегралу за формулою лівих прямокутників
+    public double CalculateLeftRectangles(double a, double b, int n, FunctionDelegate function)
+    {
+        double result = 0.0;
+        double dx = (b - a) / n;
+
+        for (int i = 0; i < n; i++)
+        {
+            double x = a + i * dx;
+            result += function(x) * dx;
+        }
+
+        return result;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Приклад використання класу для обчислення інтегралу функції x^2 від 0 до 1 з 1000 прямокутниками
+        IntegralCalculator calculator = new IntegralCalculator();
+        double result = calculator.CalculateLeftRectangles(0, 1, 1000, x => x * x);
+
+        Console.WriteLine("Результат обчислення інтегралу x^2 від 0 до 1: " + result);
+    }
+}
